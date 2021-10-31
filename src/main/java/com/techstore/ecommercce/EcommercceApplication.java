51ae88d51ce8955f4aa9be2b2b1544b678775abe
Tạ Kim Tài <takimtai123@gmail.com>
2021-10-31 23:01:49 +0700

    initial commit

---
+++ src/main/java/com/techstore/ecommercce/EcommercceApplication.java
@@ -0,0 +1,13 @@
+package com.techstore.ecommercce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techstore/ecommercce/EcommercceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstore.ecommercce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
